8b44988c7178ee69be1627d8a9cd5fadeb523f5b
Junjie <junjie.eric.guo@gmail.com>
2016-04-14 16:27:52 +1000

    Loading Animation For Main Page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,6 +3,11 @@
 
     <link rel="stylesheet" href="{{asset('assets/css/map-style.css')}}">
 
+
+
+    <link rel="stylesheet" href="{{asset('assets/spin/css/normalize.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/spin/css/main.css')}}">
+
     <style>
         .firstHead{
             color:black;
@@ -10,32 +15,25 @@
         input[date]{
             width:100%;
         }
-
         .over-lay
         {
             position:fixed; z-index:10000; margin:0; width:100%;height:auto; background:#191919; opacity:0.9;
         }
-
         .top{
             top:44px;
         }
-
         .bottom{
             bottom:0px;
         }
-
         .bottom50{
             bottom:50px;
         }
-
         .right{
             right:20px;
         }
-
         .left{
             left:20px;
         }
-
         .white
         {
             color:#dbdbdb;
@@ -44,7 +42,6 @@
             /*width:80%;*/
             margin:5px 10px;
         }
-
         .listview-icon
         {
             position:fixed;
@@ -54,71 +51,77 @@
             width:auto;
             font-weight:100;
         }
-
         :optional {
-
             opacity: 0.6;
         }
-
         :optional:hover {
             opacity: 1;
         }
-
         .round-button {
             width:14%;
             right:20px; position:fixed;
             bottom:130px;
-
         }
         .round-button-circle {
             width:100%;
             border-radius:50%;
             background-repeat:no-repeat;
-
         }
-
         .textbox {
-            -moz-border-radius-topleft: 30px;
+            /*-moz-border-radius-topleft: 30px;
             -webkit-border-top-left-radius: 30px;
             border-top-left-radius: 30px;
             -moz-border-radius-bottomleft: 30px;
             -webkit-border-bottom-left-radius: 30px;
             border-bottom-left-radius: 30px;
+
+			-moz-border-radius-topright: 0px;
+            -webkit-border-top-right-radius: 0px;
+            border-top-right-radius: 0px;
+            -moz-border-radius-bottomright: 0px;
+            -webkit-border-bottom-right-radius: 0px;
+            border-bottom-right-radius: 0px;
+			*/
+            box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            height: 100%;
+            overflow: hidden;
+            -webkit-appearance: textfield;
+            background-color: white;
+            -webkit-rtl-ordering: logical;
+            -webkit-user-select: text;
+            -webkit-box-sizing: border-box;
             border: 1px solid #848484;
             outline:0;
-            height:35px;
+            height:36px;
             width: 95%;
             padding-left:20px;
             border:none;
             -webkit-appearance: none;
         }
-
         .btn {
-            -moz-border-radius-topright: 30px;
+            /*-moz-border-radius-topright: 30px;
             -webkit-border-top-right-radius: 30px;
             border-top-right-radius: 30px;
             -moz-border-radius-bottomright: 30px;
             -webkit-border-bottom-right-radius: 30px;
-            border-bottom-right-radius: 30px;
+            border-bottom-right-radius: 30px;*/
             font-family: Arial;
             color: #ffffff;
             background: #d93434;
             padding: 8px 10px 8px 8px;
             text-decoration: none;
             border:none;
-            width:60px;
-            height:36px;
+            width:90px;
+            height:38px;
             -webkit-appearance: none;
             z-index:10000;
+            left:90%;
         }
-
-
         .btn:hover {
             background: #e60000;
             text-decoration: none;
         }
-
-
         .btn2 {
             -moz-border-radius-top: 30px;
             -webkit-border-top-radius: 30px;
@@ -135,9 +138,7 @@
             width:60px;
             height:36px;
             -webkit-appearance: none;
-
         }
-
         ::-webkit-input-placeholder { /* Chrome/Opera/Safari */
             color:#2c2c2c;
         }
@@ -152,12 +153,54 @@
         }
 
 
+        /* this is for loading page */
+        /*
+        .back-link a {
+        color: #4ca340;
+        text-decoration: none;
+        border-bottom: 1px #4ca340 solid;
+        }
+        .back-link a:hover,
+        .back-link a:focus {
+            color: #408536;
+            text-decoration: none;
+            border-bottom: 1px #408536 solid;
+        }
+        h1 {
+            height: 100%;
+            /* The html and body elements cannot have any padding or margin. */
+        /*margin: 0;
+        font-size: 14px;
+        font-family: 'Open Sans', sans-serif;
+        font-size: 32px;
+        margin-bottom: 3px;
+    }
+    .entry-header {
+        text-align: left;
+        margin: 0 auto 50px auto;
+        width: 80%;
+        max-width: 978px;
+        position: relative;
+        z-index: 10001;
+    }
+    #demo-content {
+        padding-top: 100px;
+    }*/
     </style>
+
 @stop
 
 @section('content')
 
+
+
+
+
+
     <div class="view another-view">
+
+
+
 
         <div class="navbar">
             <div class="navbar-inner">
@@ -173,8 +216,19 @@
             </div>
         </div>
 
+
+
         <!-- Pages -->
         <div class="navbar-fixed toolbar-fixed" data-page="contact">
+
+
+            <div id="loader-wrapper">
+                <div id="loader"></div>
+
+                <div class="loader-section section-left"></div>
+                <div class="loader-section section-right"></div>
+
+            </div>
 
             <div class="over-lay top">
                 <div class="forms">
@@ -241,11 +295,14 @@
         </div>
     </div>
 
-    @stop
+@stop
 
-    @section("footer")
+@section("footer")
 
-     <!--Build Vue.js -->
+    <script src="{{asset('assets/spin/js/main.js')}}"></script>
+    <script src="{{asset('assets/spin/js/vendor/modernizr-2.6.2.min.js')}}"></script>
+
+    <!--Build Vue.js -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.14/vue.min.js"></script>
     <!--Build Loading Overlay -->
     <script type="text/javascript" src="{{asset('assets/js/loadingoverlay.js')}}"></script>
@@ -254,4 +311,8 @@
     <!--Build Google Map API -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAomTWe6-_JXMoza7hm9olIQLZ8TEq5PdY&callback=app.createMap"
             async defer></script>
+
+
+
+
 @stop
